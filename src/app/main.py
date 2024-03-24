0c37544d25baa88420e9b6aeaab3c87703f4a799
duribef <diego.uribe.f@gmail.com>
2024-03-24 07:41:49 -0300

    added create_employees endpint

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import app.services as _services
+import app.schemas as _schemas
+from typing import List
+from sqlalchemy.orm import Session
+from fastapi.exceptions import HTTPException
 
 app = FastAPI()
 
 # Create tables
 _services._add_tables()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Add new data to hired_employees table
+@app.post("/api/employees/", response_model=List[_schemas.Employees])
+async def create_employees(
+    employees: List[_schemas.EmployeesCreate],
+    db: Session = Depends(_services.get_db),
+):
+    if len(employees) > 1000:
+        raise HTTPException(status_code=400, detail="Maximum batch size is 1000")
+    return await _services.create_employees(employees=employees, db=db)
--- src/app/services.py
+++ src/app/services.py
@@ -1,5 +1,31 @@
 import app.database as _database
+from typing import List
+import app.schemas as _schemas
+import app.models as _models
+from sqlalchemy.orm import Session
 
 # Create tables
 def _add_tables():
     return _database.Base.metadata.create_all(bind=_database.engine)
+
+# Database Session
+def get_db():
+    db = _database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Add Data Logic
+async def create_employees(
+    employees: List[_schemas.EmployeesCreate], db: Session
+) -> _schemas.Employees:
+    employee_objects = [_models.Employees(**employee.model_dump()) for employee in employees]
+    db.add_all(employee_objects)
+    db.commit()    
+    
+    # Refresh each individual object to update its state from the database
+    for employee in employee_objects:
+        db.refresh(employee)
+    
+    return [_schemas.Employees.model_validate(employee, from_attributes=True) for employee in employee_objects]
